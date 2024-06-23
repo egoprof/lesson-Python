3fb94234492ad383300cbfd21340ef13f22d8163
egoprof <egoprof@inbox.ru>
2024-06-23 23:33:01 +0300

    module_4_1

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-print('Hi, PyCharm')
-x = 43
-y = 32
-print(x * y)
-print("End line")
+from fake_math import fake_divide as fake_d
+from true_math import true_divide as true_d
+
+
+result1 = fake_d(69, 3)
+result2 = fake_d(3, 0)
+result3 = true_d(49, 7)
+result4 = true_d(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
