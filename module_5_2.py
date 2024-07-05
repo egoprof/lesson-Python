26d8ab697188aee51b8d21e8a0766d11c7969729
egoprof <egoprof@inbox.ru>
2024-07-06 01:29:24 +0300

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,36 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def __len__(self):
+        return (self.number_of_floors)
+    def __str__(self):
+        return (f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+
+    def go_to(self,new_floor):
+        t = new_floor > 0 and new_floor <= self.number_of_floors
+        if t:
+            for i in range (1, new_floor+1):
+                print(i)
+        else:
+            print(f'Такого этажа не существует!')
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print('----------------------------------------')
+print('задание №2, module_5_2.py')
+print('----------------------------------------')
+# __str__
+print(h1)
+print(h2)
+# __len__
+print(len(h1))
+print(len(h2))
