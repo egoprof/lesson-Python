ef83e2db13b2c6bb3e7026596d9284c41735f430
egoprof <egoprof@inbox.ru>
2024-08-12 22:26:14 +0300

    Add files via upload

---
+++ module_6_2.py
@@ -0,0 +1,50 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['red', 'green', 'blue', 'white', 'black', 'gray']
+    def __init__(self, owner: str, model: str, color='white', engine_power=500):
+        self.owner = owner
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
+
+    def get_model(self):
+        return f"Модель: {self.__model}"
+
+    def get_horsepower(self):
+        return f"Мощность двигателя: {self.__engine_power}"
+
+    def get_color(self):
+        return f"Цвет: {self.__color}"
+
+    def print_info(self):
+        print(self.get_model(), self.get_horsepower(), self.get_color(),
+              f'Владелец: {self.owner}', sep='\n', end='\n\n')
+
+    def set_color(self, new_color: str):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(bcolors.FAIL + 'Нельзя сменить цвет на,', new_color + bcolors.ENDC)
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+vehicle1.print_info()
+
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+vehicle1.print_info()
